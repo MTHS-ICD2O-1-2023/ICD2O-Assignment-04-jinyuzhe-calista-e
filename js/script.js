4e076c37ea22def567e081f3428cd0e2021dcd2b
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-05-01 15:46:42 +0000

    mj

--- js/script.js
+++ js/script.js
@@ -6,34 +6,34 @@
 "use strict"
 
 function calculate() {
-  let size = document.getElementById("size").value
-  let topping = document.getElementById("topping").value
+    let size = document.getElementById("size").value
+    let topping = document.getElementById("topping").value
 
-  let sizePrice = 0
-  let toppingPrice = 0
+    let sizePrice = 0
+    let toppingPrice = 0
 
-  if (size == "Large") {
-    sizePrice = 6.00;
-  } else if (size == "Extra Large") {
-    sizePrice = 10.00;
-  } else {
-    document.getElementById("size").innerHTML = "Error";
+    if (size == "Large") {
+        sizePrice = 6.00;
+    } else if (size == "Extra Large") {
+        sizePrice = 10.00;
+    } else {
+        document.getElementById("size").innerHTML = "Error";
   }
 
-  if (usertopping == "onion") {
-    toppingPrice = 1.00;
-  } else if (usertopping == "tomatoes") {
-    toppingPrice = 1.75;
-  } else if (usertopping == "ham") {
-    toppingPrice = 2.50;
-  } else if (usertopping == "pineapple") {
-    toppingPrice = 3.35;
-  }else {
-    document.getElementById("size").innerHTML = "Error";
-  }
+    if (usertopping == "onion") {
+        toppingPrice = 1.00;
+    } else if (usertopping == "tomatoes") {
+        toppingPrice = 1.75;
+    } else if (usertopping == "ham") {
+        toppingPrice = 2.50;
+    } else if (usertopping == "pineapple") {
+        toppingPrice = 3.35;
+    }else {
+        document.getElementById("size").innerHTML = "Error";
+    }
 
-  let totalPrice = ( sizePrice + toppingPrice ) * 0.13 + ( sizePrice + toppingPrice );
+    let totalPrice = ( sizePrice + toppingPrice ) * 0.13 + ( sizePrice + toppingPrice );
 
-  document.getElementById("total").innerHTML =
+    document.getElementById("total").innerHTML =
     "Your total will be $" + totalPrice + "! Enjoy your pizza!"
 }
